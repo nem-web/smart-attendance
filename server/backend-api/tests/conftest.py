3f0aa4fc4556f07c71b7b34082e2db4a8136154c
Maaz Jatt <maazcoding54@gmail.com>
2026-03-06 22:17:21 +0500

    fixed tests

--- server/backend-api/tests/conftest.py
+++ server/backend-api/tests/conftest.py
@@ -146,8 +146,9 @@ async def client(db):
     """
     Async client for API requests.
     """
-    from app.main import app
+    from app.main import create_app
     from app.core.limiter import limiter
+    app = create_app()
 
     # Disable rate limiting for tests (redundant with autouse but safe)
     limiter.enabled = False
@@ -156,7 +157,7 @@ async def client(db):
     # app.db.mongo.db should point to 'test_smart_attendance' because of env var.
 
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
+        transport=ASGITransport(app=app), base_url="http://test/api/v1"
     ) as ac:
         yield ac
 
@@ -215,9 +216,15 @@ async def auth_token(client, db, test_user_data):
         "email": test_user_data["email"],
         "password": test_user_data["password"],
     }
-    response = await client.post("/auth/login", json=login_data)
-    return response.json()["token"]
 
+    response = await client.post("/auth/login", json=login_data)
+
+    print("STATUS:", response.status_code)
+    print("BODY:", response.json())
+
+    
+    return response.json()["token"]
+ 
 
 @pytest.fixture
 def make_token_header():
